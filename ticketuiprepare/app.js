37016cddd66a8565f3f31e0dc182ad732a0cfaf2
aungkyawzaww <aungkyawzaww7889@gmail.com>
2024-09-05 21:28:05 +0630

    5.9.24

--- ticketuiprepare/app.js
+++ ticketuiprepare/app.js
@@ -2,7 +2,25 @@ $(document).ready(function(){
 
     console.log('hay');
 
+    $("#calendar").click(function(){
 
-    $('#bookingdate').datepicker();
+        $('#bookingdate').datepicker();
 
+    });
+
+    $("#closebtn").click(function(){
+        $('#bookingdate').datepicker("hide");
+        console.log("hay"); 
+    })
+    
+
+    let getdate = new Date();
+    let getday = getdate.getDay();
+    let getmonth = getdate.getMonth();
+    let getyear = getdate.getUTCFullYear();
+
+    console.log(getday,getmonth,getyear);
+
+    console.log($('#bookingdate').attr("value"));
 });
+
