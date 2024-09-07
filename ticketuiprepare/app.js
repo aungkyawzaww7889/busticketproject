cb3bc2523761ae33ffc096c94d37391136b5adb1
aungkyawzaww <aungkyawzaww7889@gmail.com>
2024-09-07 11:22:51 +0630

    update 7/9/24

--- ticketuiprepare/app.js
+++ ticketuiprepare/app.js
@@ -1,26 +1,84 @@
 $(document).ready(function(){
 
-    console.log('hay');
+    let today = new Date();
+    let day = today.getDate() + 7;
+    let month = today.getMonth() + 1; // Months are zero-indexed, so January is 0
+    let year = today.getFullYear();
+    
+    // Format the date as needed (e.g., DD/MM/YYYY)
+    let formattedDate = month + '/' + day + '/' + year;
 
-    $("#calendar").click(function(){
+    // console.log('Today\'s Date: ' + formattedDate);
 
-        $('#bookingdate').datepicker();
-
+    $('#bookingdate').datepicker({
+        minDate:0,
+        showAnim:"slideDown",
+        duration:"slow",
+        maxDate:`${formattedDate}`
     });
 
-    $("#closebtn").click(function(){
-        $('#bookingdate').datepicker("hide");
-        console.log("hay"); 
-    })
-    
+    $('#bookingdate').change(function(){
+        // console.log($("#deparature").text());
+        $("#deparature").css({
+            opacity:"0"
+        });
+    });
 
-    let getdate = new Date();
-    let getday = getdate.getDay();
-    let getmonth = getdate.getMonth();
-    let getyear = getdate.getUTCFullYear();
-
-    console.log(getday,getmonth,getyear);
-
-    console.log($('#bookingdate').attr("value"));
 });
+
+
+
+const getminus = document.getElementById('minus');
+const getplus = document.getElementById('plus');
+const getnum = document.getElementById('num');
+
+
+
+let x = 0;
+getplus.addEventListener('click',function(){
+
+    x++;
+    if(x >= 4){
+        x = 4;
+        getplus.style.opacity = 0.5;
+    }
+
+    if(x >= 1){
+        getnum.innerText = `${x} Seats`;
+        getminus.style.opacity = 1;
+
+    }else{
+        getnum.innerText = `${x} Seat`;
+
+    }
+
+    // console.log(x);
+
+});
+
+
+
+getminus.addEventListener('click',function(){
+
+    x--;
+    if(x <= 0){
+        x = 0;
+        getminus.style.opacity = 0.5;
+    }
+
+    if(x >= 1){
+        getnum.innerText = `${x} Seats`;
+        getplus.style.opacity = 1;
+    }else{
+        getnum.innerText = `${x} Seat`;
+    }
+
+    // console.log(x);
+
+
+
+});
+
+
+
 
